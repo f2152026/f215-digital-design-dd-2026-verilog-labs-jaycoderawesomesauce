269da5f98d3a8077719df488b2ffa01ca4cbc7cb
JayCoderAwesomeSauce <itsjayroy123@gmail.com>
2026-09-19 09:18:37 +0000

    lab02 task1

--- labs/lab02/task1/dut.v
+++ labs/lab02/task1/dut.v
@@ -26,4 +26,5 @@ module DUT (
   //   .Y  (Y)
   // );
 
+ 
 endmodule
--- labs/lab02/task1/mux_df.v
+++ labs/lab02/task1/mux_df.v
@@ -9,8 +9,11 @@ module mux_df (
   input      I0,
   input      I1,
   input      S,
-  output reg Y
+  output Y
 );
+   
+    
+     
 
   assign Y = S ? I1 : I0;
 
